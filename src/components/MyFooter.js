223d524f11a3d1fd4672cfd3f4a0f9651a4123f0
Park <angle307@naver.com>
2023-09-15 00:06:16 +0900

    [ADD] footer

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,46 +1,32 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const MyFooter = () => {
   return (
     <footer className="Footer">
       <div className="left">
         <div className="head">
           <img src={process.env.PUBLIC_URL + "images/book.png"} alt="logo" />
-          <h2>
-            <a href="https://github.com/DT-HYUNJUN/munjang-space" target="_blank" rel="noreferrer">
-              문장의 공간
-            </a>
-          </h2>
+          <span>문장의 공간</span>
         </div>
-        <p className="copyright">&copy; 2023 중임무황태 All Rights Reserved.</p>
+        <div className="description">
+          <span>"문장의 공간"은 글을 읽고 쓰는 과정을 즐기는 모든 이들을 위한 플랫폼입니다.</span>
+          <span> 함께 글을 읽고 쓰며 새로운 창조적 경험을 만나보세요.</span>
+          <span>우리는 다양한 글쓰기 형식과 주제를 사랑하며, 이를 공유하고 협력하는 공동체를 형성합니다.</span>
+        </div>
+        <span className="copyright">
+          &copy; 2023 <span className="project_title">중임무황태</span> All Rights Reserved.
+        </span>
       </div>
-
-      <div className="content_wrapper">
+      <div className="right">
+        <span className="text">Members</span>
         <a className="content" href="https://github.com/DT-HYUNJUN" target="_blank" rel="noreferrer">
-          <svg
-            height="18"
-            aria-hidden="true"
-            viewBox="0 0 16 16"
-            version="1.1"
-            width="18"
-            data-view-component="true"
-            class="octicon octicon-mark-github v-align-middle color-fg-default"
-          >
-            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-          </svg>
-          <span>박현준</span>
+          <FontAwesomeIcon icon={faGithub} />
+          <span className="name">박현준</span>
         </a>
         <a className="content" href="https://github.com/badajinsee" target="_blank" rel="noreferrer">
-          <svg
-            height="18"
-            aria-hidden="true"
-            viewBox="0 0 16 16"
-            version="1.1"
-            width="18"
-            data-view-component="true"
-            class="octicon octicon-mark-github v-align-middle color-fg-default"
-          >
-            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-          </svg>
-          <span>한선진</span>
+          <FontAwesomeIcon icon={faGithub} />
+          <span className="name">한선진</span>
         </a>
       </div>
     </footer>
