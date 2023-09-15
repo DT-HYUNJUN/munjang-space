57e561b886988f7cbc7696a24a20923372b79af5
sunjin <changido2888@gmail.com>
2023-09-15 16:44:59 +0900

    [ADD] login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,82 @@
+import styled from "styled-components";
+import MyButton from "./MyButton";
+
+const Title = styled.h2`
+  padding-left: 30px;
+  padding-right: 30px;
+
+  margin-bottom: 5px;
+
+  text-align: center;
+  font-family: "UhBeeJJIBBABBA";
+  font-size: 30px;
+`;
+
+const Subtitle = styled.p`
+  padding-left: 30px;
+  padding-right: 30px;
+
+  margin-top: 0px;
+
+  text-align: center;
+  font-family: "KyoboHandwriting2021sjy";
+  font-size: 20px;
+  color: gray;
+`;
+
+const LoginForm = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
+const Logininput = styled.input`
+  border: 1px solid gray;
+  border-radius: 3px;
+
+  background-color: #ececec;
+  color: black;
+
+  width: 400px;
+  height: 40px;
+
+  margin-bottom: 10px;
+`;
+
+const Login_1 = styled.div`
+  display: grid;
+`;
+
 const Login = () => {
-  return <h2>로그인 페이지</h2>;
+  return (
+    <div>
+      <div>
+        <Title> 로그인 </Title>
+        <Subtitle>문장의 공간과 함께 하세요.</Subtitle>
+      </div>
+
+      <LoginForm>
+        <Login_1>
+          <Logininput
+            type="text"
+            textarea
+            name="userName"
+            placeholder="Email"
+          />
+          <Logininput
+            type="password"
+            name="userPassword"
+            placeholder="Password"
+          />
+        </Login_1>
+
+        <label htmlFor="remember-check">
+          <input type="checkbox" id="remember-check" /> 아이디 저장하기
+        </label>
+
+        <MyButton text={"로그인"} type={"positive"} onClick={onclick} />
+      </LoginForm>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -10,21 +10,38 @@ const MyFooter = () => {
           <span>문장의 공간</span>
         </div>
         <div className="description">
-          <span>"문장의 공간"은 글을 읽고 쓰는 과정을 즐기는 모든 이들을 위한 플랫폼입니다.</span>
+          <span>
+            "문장의 공간"은 글을 읽고 쓰는 과정을 즐기는 모든 이들을 위한
+            플랫폼입니다.
+          </span>
           <span> 함께 글을 읽고 쓰며 새로운 창조적 경험을 만나보세요.</span>
-          <span>우리는 다양한 글쓰기 형식과 주제를 사랑하며, 이를 공유하고 협력하는 공동체를 형성합니다.</span>
+          <span>
+            우리는 다양한 글쓰기 형식과 주제를 사랑하며, 이를 공유하고 협력하는
+            공동체를 형성합니다.
+          </span>
         </div>
         <span className="copyright">
-          &copy; 2023 <span className="project_title">중임무황태</span> All Rights Reserved.
+          &copy; 2023 <span className="project_title">중임무황태</span> All
+          Rights Reserved.
         </span>
       </div>
       <div className="right">
         <span className="text">Members</span>
-        <a className="content" href="https://github.com/DT-HYUNJUN" target="_blank" rel="noreferrer">
+        <a
+          className="content"
+          href="https://github.com/DT-HYUNJUN"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon icon={faGithub} />
           <span className="name">박현준</span>
         </a>
-        <a className="content" href="https://github.com/badajinsee" target="_blank" rel="noreferrer">
+        <a
+          className="content"
+          href="https://github.com/badajinsee"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FontAwesomeIcon icon={faGithub} />
           <span className="name">한선진</span>
         </a>
