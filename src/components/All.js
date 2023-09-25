9f09484500c9c7871f3eead68e8c4bbc9b780003
sunjin <changido2888@gmail.com>
2023-09-26 00:37:40 +0900

    [ADD] 별점

--- src/components/All.js
+++ src/components/All.js
@@ -1,5 +1,18 @@
-const All = () => {
-  return <div>dd</div>;
+const All = ({ reportList }) => {
+  return (
+    <div>
+      {reportList.map((item) => (
+        <div key={item.id}>
+          <p>bookname: {item.bookname}</p>
+          <p>title: {item.title}</p>
+          <p>content: {item.content}</p>
+          <p>date: {item.date}</p>
+          <p>private: {item.private ? "참" : "거짓"}</p>
+          <span>--</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default All;
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import styled from "styled-components";
 
 import All from "../components/All";
 import Month from "../components/Month";
+import MyButton from "../components/MyButton";
 
 const SwitchButton = styled.div``;
 
@@ -45,11 +47,63 @@ const MonthButton = styled.button`
   }
 `;
 
-const List = () => {
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const sortStarOptionList = [
+  { value: "allStar", name: "전부" },
+  { value: 1, name: "1" },
+  { value: 2, name: "2" },
+  { value: 3, name: "3" },
+  { value: 4, name: "4" },
+  { value: 5, name: "5" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const List = ({ reportList }) => {
+  const navigate = useNavigate();
   const [all, setAll] = useState(false);
 
   const onAllHandler = () => setAll(true);
   const onMonthHandler = () => setAll(false);
+
+  const [sortType, setSortType] = useState("latest");
+
+  const [filterStar, setFilterStar] = useState("allStar");
+
+  const getProcessReportList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    const copyList = JSON.parse(JSON.stringify(reportList));
+
+    const filterdStarList =
+      filterStar === "allStar"
+        ? copyList
+        : copyList.filter((it) => parseInt(it.star) === parseInt(filterStar));
+
+    const sortedList = filterdStarList.sort(compare);
+
+    return sortedList;
+  };
 
   return (
     <div>
@@ -57,7 +111,32 @@ const List = () => {
         <AllButton onClick={onAllHandler}>전체</AllButton>
         <MonthButton onClick={onMonthHandler}>월별</MonthButton>
       </SwitchButton>
-      {all ? <All /> : <Month />}
+
+      <div>
+        <ControlMenu
+          value={sortType}
+          onChange={setSortType}
+          optionList={sortOptionList}
+        />
+
+        <ControlMenu
+          value={filterStar}
+          onChange={setFilterStar}
+          optionList={sortStarOptionList}
+        />
+
+        <MyButton
+          type={"positive"}
+          text={"새 독후감 작성하기"}
+          onClick={() => navigate("/new")}
+        />
+
+        {all ? (
+          <All reportList={getProcessReportList()} />
+        ) : (
+          <Month reportList={getProcessReportList()} />
+        )}
+      </div>
     </div>
   );
 };
