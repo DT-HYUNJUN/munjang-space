640cc06784ecc8ccc4ca52b17e35576b015c766e
Park <angle307@naver.com>
2023-10-28 14:17:00 +0900

    [UPDATE] home best seller books carousel

--- src/utils/listBooks.js
+++ src/utils/listBooks.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const listBooks = async () => {
   const key = "ttbangle3071358001";
-  const res = await axios.get(
-    `/ttb/api/ItemList.aspx?ttbkey=${key}&QueryType=Bestseller&Cover=Mid&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`
-  );
+  const res = await axios.get(`/ttb/api/ItemList.aspx?ttbkey=${key}&QueryType=Bestseller&Cover=Big&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`);
   return res.data.item;
 };
 
