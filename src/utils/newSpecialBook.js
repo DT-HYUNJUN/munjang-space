18f0bed73e2138b987bd388cf23422e648d052ce
Park <angle307@naver.com>
2023-11-03 01:56:20 +0900

    [UPDATE] gitignore

--- src/utils/newSpecialBook.js
+++ src/utils/newSpecialBook.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-// const newSpecialBook = async () => {
-//   const key = "ttbangle3071358001";
-//   const res = await axios.get(`/ttb/api/ItemList.aspx?ttbkey=${key}&QueryType=ItemNewSpecial&Cover=Big&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`);
-//   return res.data.item;
-// };
-
 const newSpecialBook = async () => {
   const proxy = "https://port-0-munjang-space-proxy-server-17xqnr2algn95l6h.sel3.cloudtype.app";
   const res = await axios.get(`${proxy}/newSpecialBook`);
