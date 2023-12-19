2ea011e3e54f2bbd4870c8ea427b55993dd53360
Park <angle307@naver.com>
2023-12-20 00:39:16 +0900

    [UPDATE] updated proxy server: http -> https

--- src/utils/getBooks.js
+++ src/utils/getBooks.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getBooks = async (inputTitle) => {
   // const proxy = "https://port-0-munjang-space-proxy-server-17xqnr2algn95l6h.sel3.cloudtype.app";
-  const proxy = "http://hlfoatnips.us14.qoddiapp.com";
+  const proxy = "https://hlfoatnips.us14.qoddiapp.com";
   const res = await axios.get(`${proxy}/getBooks?title=${inputTitle}`);
   return res.data;
 };
